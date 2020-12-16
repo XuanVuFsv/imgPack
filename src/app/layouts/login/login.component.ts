11fc2ebbbc4a6c3916f8f4227caffda8310d90d9
dugkiet <dugkiet@gmail.com>
2020-12-16 10:51:35 +0700

    login

--- src/app/layouts/login/login.component.ts
+++ src/app/layouts/login/login.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+
+    console.log("Comment");
   }
 
 }
